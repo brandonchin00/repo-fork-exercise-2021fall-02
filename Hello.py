708b561d9773d1fe236a956d2548a2a79ab6e962
Brandon <brandonzchin@gmail.com>
2021-09-09 22:44:35 -0400

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,1 +1,37 @@
-print ("hello")
+from typing import Match
+
+
+name_1 = "Isabella Sounthard"
+
+b = len(name_1)
+
+name_2 = 'Jonathan Cavazos'
+
+a = len(name_2)
+
+c = len(str(2 ** 1_000_000))
+
+#produces a very large number do not try to do it again. Lags computer for a second
+#a_very_large_number = 2 ** 100000
+
+#print(math.sqrt(4))
+
+import random
+
+print(a, b)
+
+print('I\'M \"ok\".')
+print('I\'m learning \nPython. ')
+print('\\\t\\')
+print(r'\\\t\\')
+
+lyrics = '''Hey Jude
+don\'t make it bad trake a sad song'''
+
+print(lyrics)
+
+age = int(input("Please enter your age: >>> "))
+if age >= 21:
+        print ('Yes, you can.')
+else:
+        print ('Sorry, you cannot')
